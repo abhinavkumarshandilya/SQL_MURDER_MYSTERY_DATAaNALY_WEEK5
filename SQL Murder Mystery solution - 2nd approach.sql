83ccb8e03e88cdc6177be381b953e41c1ac6a295
Abhinav Kumar <141053732+abhinavkumarshandilya@users.noreply.github.com>
2024-01-07 20:17:22 +0530

    Update SQL Murder Mystery solution - 2nd approach.sql

--- SQL Murder Mystery solution - 2nd approach.sql
+++ SQL Murder Mystery solution - 2nd approach.sql
@@ -4,7 +4,7 @@ solve an intriguing crime."
 
 Experienced SQL sleuths start here:
 A crime has taken place and the detective needs your help. The detective gave you the crime scene report, but you somehow lost it. You vaguely remember that the crime was a ​murder​ that 
-occurred sometime on ​Jan.15, 2018​ and that it took place in ​SQL City​. Start by retrieving the corresponding crime scene report from the police department’s database.
+occurred sometime on ​Jan.5, 2024​ and that it took place in ​SQL City​. Start by retrieving the corresponding crime scene report from the police department’s database.
 
 Exploring the Database Structure:
 Experienced SQL users can often use database queries to infer the structure of a database. But each database system has different ways of managing this information. 
